4d9154cfa243359818b1eefb9fae0864ea094bbc
Abyan Rahman Al Fariz <117020526+Rarizz@users.noreply.github.com>
2024-10-02 14:01:23 +0700

    Add files via upload

---
+++ 03_I0_Tipe_Data_dan_Variabel/UNGUIDED/PendahuluanModul3/Soal3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		Fahrenheit float32
+		Kelvin float32
+	)
+
+	fmt.Println("Masukan suhu awal dalam derajat Fahrenheit")
+	fmt.Scan(&Fahrenheit)
+	
+	//rumus konversi dari Fahrenheit ke Kelvin
+	Kelvin = (Fahrenheit-32)*5/9 + 273.15
+
+	fmt.Println("Derajat suhu dalam kelvin adalah", Kelvin)
+}
